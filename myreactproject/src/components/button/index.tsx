8934edca94645ce5e8f322f8cef2c000081990fe
Math-ARod <matheusvwg@gmail.com>
2023-07-27 09:37:29 -0300

    chore: create test button

---
+++ myreactproject/src/components/button/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Aqui criamos um botão e extendemos ele a um componente React. Ou seja, agora éle é um componente do react
+class Button extends React.Component {
+    render() {
+        return (
+            <button>
+                Botão
+            </button>
+        )
+    }
+}
+
+export default Button
