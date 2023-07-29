f3fe89c2838730d683b4de985b7ed26018aa0aad
Math-ARod <matheusvwg@gmail.com>
2023-07-29 09:13:27 -0300

    chore: create List component

--- myreactproject/src/App.tsx
+++ myreactproject/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GuestCard from './components/guestcard';
+import List from './components/list';
 
 function App() {
   return (
     <div className="App">
       <GuestCard/>
+      <List/>
     </div>
   );
 }
